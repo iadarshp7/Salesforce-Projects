c8a8724839253140c0847d18d1dfb308ac99c69f
Adarsh Pal <adarshpal8707@gmail.com>
2025-09-03 16:53:28 +0530

    Add carousel functionality to Student component and bind data from School component

--- force-app/main/default/lwc/school/school.js
+++ force-app/main/default/lwc/school/school.js
@@ -1,4 +1,21 @@
 import { LightningElement } from 'lwc';
 
 export default class School extends LightningElement {
+    carousedDetails = [
+        {
+            src: "https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
+            header: "First Card",
+            description: "First card description."
+        },
+        {
+            src: "https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg",
+            header: "Second Card",
+            description: "Second card description."
+        },
+        {
+            src: "https://v1.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",
+            header: "Third Card",
+            description: "Third card description."
+        }
+    ];
 }
--- force-app/main/default/lwc/student/student.js
+++ force-app/main/default/lwc/student/student.js
@@ -2,4 +2,5 @@ import { LightningElement, api } from 'lwc';
 
 export default class Student extends LightningElement {
     @api name;
+    @api carouselData;
 }
